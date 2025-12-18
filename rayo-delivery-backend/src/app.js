2aa2b8f59990d18c26dee9a289fa4244c4eb390b
Jack A. Aymara Enciso <jackae598@gmail.com>
2025-12-18 00:56:30 -0500

    Chore: Ajustes generales en la configuración del backendSe realizaron mejoras menores en la configuración principal del servidor para asegurar un funcionamiento más estable y alineado con los módulos actualizados.

--- rayo-delivery-backend/src/app.js
+++ rayo-delivery-backend/src/app.js
@@ -13,11 +13,13 @@ app.use(cors());
 const localRoutes = require("./routes/localRoutes");
 const productoRoutes = require("./routes/productRoutes");
 const authRoutes = require("./routes/authRoutes"); // <-- aquí
+const pedidoRoutes = require("./routes/pedidoRoutes");
 
 // Montar rutas con prefijo /api
 app.use("/api/locales", localRoutes);
 app.use("/api/productos", productoRoutes);
 app.use("/api/auth", authRoutes); // <-- aquí
+app.use("/api/pedidos", pedidoRoutes);
 
 const PORT = 4000;
 app.listen(PORT, () => {
