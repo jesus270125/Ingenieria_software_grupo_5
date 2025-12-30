a372a1944af9cfe7794e7815b97a20273b7bc94f
Jesus Alejandro Moron Luna <231188@unamba.edu.pe>
2025-12-30 10:03:15 -0500

    Chore: Vaciar repositorio manteniendo historial

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Repo vaciado intencionalmente" 
--- rayo-delivery-backend/src/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/searchController");
-
-// No requiere login
-router.get("/", controller.buscar);
-
-module.exports = router;
